e17deab2e1d6c5d212939cec5a44b260cd6b3f91
Rosa Etombayambo <34574707+rose2k17@users.noreply.github.com>
2018-03-12 14:48:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import EmployeeDirectoryApp from './app/EmployeeDirectoryApp';
+
+AppRegistry.registerComponent('EmployeeDirectory', () => EmployeeDirectoryApp);
